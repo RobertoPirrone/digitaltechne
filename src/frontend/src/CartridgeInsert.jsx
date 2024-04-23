147d168ac2d5ade69259dfd8fd8c4f08f2202091
Roberto Pirrone <roberto.pirrone@most.it>
2024-04-23 15:22:32 +0200

    xls -> json

--- src/frontend/src/CartridgeInsert.jsx
+++ src/frontend/src/CartridgeInsert.jsx
@@ -7,6 +7,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
+import * as XLSX from "xlsx";
 
 import { Header } from "./Header";
 import { Footer } from "./Footer";
@@ -22,20 +23,43 @@ export const CartridgeInsert = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const [pdfAsset, setPdfAsset] = useState({});
+  const [xlsAsset, setXlsAsset] = useState({});
   const [disabledButs, setDisabledButs] = useState(true);
   const [searchele, setSearchele] = useState(false);
   const [note, setNote] = useState("");
   const [dnaText, setDnaText] = useState("");
+    const [file, setFile] = useState(null);
+  const [jsonData, setJsonData] = useState("");
+
+
+    const Xls2Json = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "binary" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = XLSX.utils.sheet_to_json(worksheet);
+        setJsonData(JSON.stringify(json, null, 2));
+      };
+      reader.readAsBinaryString(file);
+    }
+  };
 
   const onSubmit = (vals) => {
-    if (pdfAsset == {}) {
-      appAlert("File Pdf Analisi non scelto");
+    if (! file ) {
+      appAlert("File Xls Analisi non scelto");
       return;
     }
-
+      Xls2Json();
     vals.uuid = uuidv4();
-    vals.dna_text = dnaText;
-    vals.dna_file_asset = pdfAsset.key;
+    vals.dna_text = jsonData;
+      if (pdfAsset == {} ) {
+        vals.dna_file_asset = "";
+      } else {
+        vals.dna_file_asset = pdfAsset.key;
+      }
     vals.lab_name = "Laboratorio CNR Catania";
     vals.note = note;
 
@@ -74,7 +98,14 @@ export const CartridgeInsert = () => {
       <h1>{t("CartridgeInsert")}</h1>
       <Container component="main" maxWidth="md">
         <div className={DTRoot}>
-          <Upload asset={pdfAsset} setAsset={setPdfAsset} setDisabledButs={setDisabledButs} />
+            <Grid container spacing={1} alignItems="center">
+                <Grid item xs={6}> <span className="padding10">{t("DnaFileXls")}</span></Grid>
+                <Grid> <input type="file" accept=".xls,.xlsx" onChange={(e) => setFile(e.target.files[0])} /> </Grid>
+
+                <Grid item xs={6}> <span className="padding10">{t("DnaFilePdf")}</span></Grid>
+                <Grid item xs={6}> <Upload asset={pdfAsset} setAsset={setPdfAsset} setDisabledButs={setDisabledButs} accept={"application/pdf"} show={false} /> </Grid>
+                <Grid item xs={12}> {" "} &nbsp; </Grid>
+      </Grid>
 
           <form onSubmit={handleSubmit(onSubmit)}>
             <Grid container spacing={1} alignItems="center">

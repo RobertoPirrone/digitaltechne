fd3a7130e79cb9db8b99329ec4e10b36d199db82
Roberto Pirrone <roberto.pirrone@most.it>
2024-05-16 09:28:41 +0200

    roba inutile, da buttare

--- src/frontend/src/ko/SignIn.jsx
+++ src/frontend/src/ko/SignIn.jsx
@@ -1,9 +1,9 @@
-//https://forum.dfinity.org/t/dfinity-auth-client-sessionstorage/12400
-//https://erxue-5aaaa-aaaab-qaagq-cai.ic0.app/auth-client/interfaces/AuthClientStorage.html
+// https://forum.dfinity.org/t/dfinity-auth-client-sessionstorage/12400
+// https://erxue-5aaaa-aaaab-qaagq-cai.ic0.app/auth-client/interfaces/AuthClientStorage.html
 // https://github.com/dfinity/agent-js/tree/main/packages/auth-client
 // https://forum.dfinity.org/t/keeping-user-logged-in-between-browser-refreshes-or-how-to-stringify-authclient-agent-actor/15035/16
 
-import {useEffect} from 'react';
+import {useContext, useEffect} from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -11,24 +11,23 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useTranslation, Trans } from 'react-i18next';
 import { AuthClient } from "@dfinity/auth-client";
 import { Actor, HttpAgent } from "@dfinity/agent";
-import { Principal } from "@dfinity/principal";
-import { useGlobalState } from './state';
 import logo from '/DT-noalpha.png';
 import { DTPaper, DTForm, DTSubmit } from './components/useStyles';
 import { MostSubmitButton} from './components/MostComponents';
 import { canisterId, idlFactory, backend } from "../../declarations/backend";
+import { myContext } from './components/MyContext';
 
 export const Login = () => {
     const navigate = useNavigate();
     const { t, i18n } = useTranslation();
     const IIcanisterId = process.env.CANISTER_ID_INTERNET_IDENTITY;
-    const [username, setUsername] = useGlobalState('username');
-    const [backend, setBackend] = useGlobalState('backend');
-    const [backendActor, setBackendActor] = useGlobalState('backendActor');
     const infoUrl = "/html/MISC/first_info.html";
+    const [ whoami, setWhoami, backendActor, setBackendActor, assetPfx, setAssetPfx ] = useContext(myContext);
+    console.log("Richiamato LOGIN");
 
     async function InternetIdentityLogin() {
-        let authClient = await AuthClient.create();
+        let authClient = await AuthClient.create({ idleOptions: { disableIdle: true } });
+        authClient.logout();
         await new Promise((resolve) => {
             authClient.login({
                 // 7 days in nanoseconds
@@ -49,11 +48,13 @@ export const Login = () => {
             if (process.env.DFX_NETWORK !== "ic") {
                     agent.fetchRootKey();
             }
-            console.log("Login prima di localStorage.setItem: ", JSON.stringify(actor));
-            setBackend(actor);
+                console.log("InternetIdentityLogin, pre whoami");
+            actor.whoami().then((Ok_data) =>  {
+                console.log("InternetIdentityLogin, whoami returns: ",Ok_data);
+                setWhoami(Ok_data);
+                localStorage.setItem("whoami", Ok_data);
+            })
             setBackendActor(actor);
-            localStorage.setItem("backendActor", JSON.stringify(actor));
-            localStorage.setItem("identity", identity);
             navigate("/dossier");
         });
     }
@@ -61,15 +62,8 @@ export const Login = () => {
     return (
     <Container component="main" maxWidth="xs">
       <div className={DTPaper}>
-        {/*
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        */}
-        <img src={logo} className="XXXApp-logo" alt="logo" />
-        <Typography component="h1" variant="h5">
-          <Trans i18nKey="techneTitle" />
-        </Typography>
+        <img src={logo} width={400} className="XXXApp-logo" alt="logo" />
+        <Typography component="h1" variant="h5"> <Trans i18nKey="techneTitle" /> </Typography>
         <div className="w-1/2 mb-4"> 
           <MostSubmitButton onClick={async () => await InternetIdentityLogin()} className="makeStyles-submit-4" label={t('SignIn.Sign In')}/>
         </div>
@@ -87,71 +81,61 @@ export const Login = () => {
   ) 
 };
 
-export const Logout = () => {
-        // let authClient = await AuthClient.create();
-    // await authClient.logout({
-      // onSuccess: () =>{
-        // console.log("logged out");
-      // }
-    // })
+export const Logout = async () => {
+  const [ whoami, setWhoami, backendActor, setBackendActor, assetPfx, setAssetPfx ] = useContext(myContext);
+  const navigate = useNavigate();
+  let authClient = await AuthClient.create();
 
-    const [username, setUsername] = useGlobalState('username');
-    const [backendActor, setBackendActor] = useGlobalState('backendActor');
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        setUsername("");
+  useEffect(() => {
         setBackendActor(null);
-        localStorage.removeItem("backendActor");
-        navigate("/login");
-    })
+        setWhoami("");
+        localStorage.setItem("whoami", "");
+        authClient.logout();
 
-  // This fix a "sign in -> sign out -> sign in again" flow without window reload.
-  // authClient = null;
+        navigate("/login");
+  })
 };
 
 
-export const checkLoggedUser = () => {
-  const [username, setUsername] = useGlobalState('username');
-  // const [backendActor, setBackendActor] = useGlobalState('backendActor');
-  const backendActor = getBackendActor();
-    console.log("checkLoggedUser backendActor: ", JSON.stringify(backendActor));
-    console.log("checkLoggedUser backendActor: ", backendActor);
-    backendActor.whoami().then((Ok_data) =>  {
-        console.log("whoami returns: ",Ok_data);
-        setUsername(Ok_data);
-        return Ok_data;
-    })
+export function checkLoggedUser () {
+    const navigate = useNavigate();
+    return localStorage.getItem("whoami");;
 }
 
-export const getBackend = () => {
-    const [backend, setBackend] = useGlobalState('backend');
-    if (backend !== null ) {
-        return backend
-    } else {
-    (async () => {
-      const client = await AuthClient.create();
-      if (client.isAuthenticated()) {
-          console.log("backend is ", backend);
-                if (backend === null || backend == "") {
-            const identity = client.getIdentity();
-            const actor = Actor.createActor(idlFactory, {
-              agent: new HttpAgent({
-                identity,
-              }),
-              canisterId,
-            });
-      setBackend(actor);
-      return actor;
-                }
-      } else {
-                    async () => await InternetIdentityLogin();
-      setBackend(actor);
-      return actor;
-      }
-    })();
-    }
-}
 export const getBackendActor = () => {
-    return backend
+    const navigate = useNavigate();
+    const [ backendActor, setBackendActor, whoami, setWhoami ] = useContext(myContext);
+    console.log("ENTRO in getBackendActor, whoami: ", whoami);
+    if (whoami == "2vxsx-fae")
+        navigate("/login");
+    console.log("ENTRO in getBackendActor: ", backendActor);
+    useEffect(() => {
+        // la identity esiste, ricreo Actor
+        if (backendActor === null || backendActor === "" ) {
+            console.log("DENTRO useEffect di getBackendActor");
+            AuthClient.create({ idleOptions: { disableIdle: true } })
+            .then((authClient) => {
+                const identity = authClient.getIdentity();
+                let agent = new HttpAgent({ identity, });
+                const actor = Actor.createActor(idlFactory, {
+                  agent: agent,
+                  canisterId,
+                });
+                console.log("IIprincipal: ",  identity.getPrincipal().toText());
+                if (process.env.DFX_NETWORK !== "ic") {
+                        agent.fetchRootKey();
+                }
+                actor.whoami().then((Ok_data) =>  {
+                    console.log("getBackendActor, whoami returns: ",Ok_data);
+                    setWhoami(Ok_data);
+                    localStorage.setItem("whoami", Ok_data);
+                })
+                setBackendActor(actor);
+            })
+        }
+    })
+
+    return backendActor;
+
 }
+

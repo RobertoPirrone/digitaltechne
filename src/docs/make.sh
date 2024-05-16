8b2a2c28833628ea31e6558b00dab5ebc4e61329
Roberto Pirrone <roberto.pirrone@most.it>
2024-05-16 12:39:16 +0200

    alabaster per pagine singole

--- src/docs/make.sh
+++ src/docs/make.sh
@@ -1,6 +1,10 @@
-sphinx-build -M html docs docs/_build
+sphinx-build -D html_theme=alabaster -D html_theme_options.nosidebar=True docs docs/_build/html
 rm -rf ../frontend/public/html
 cp -a docs/_build/html ../frontend/public/
+
+sphinx-build -M html docs docs/rtd_build
+rm -rf ../frontend/public/rtd_html
+cp -a docs/rtd_build/html ../frontend/public/rtd_html
 
 sphinx-build -b rinoh docs docs/_build/rinoh
 cp -a docs/_build/rinoh/UserManual.pdf ../frontend/public/

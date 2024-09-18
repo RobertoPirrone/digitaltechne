a7afda3e4846de798fe30126da44263df0b43d0c
Roberto Pirrone <roberto.pirrone@most.it>
2024-09-18 11:39:23 +0200

    abbozzato backend gramberg

--- to_local_restart.sh
+++ to_local_restart.sh
@@ -1,16 +1,27 @@
+# npm install -D typescript (per tsc)
+# dfx start --background --clean
+
 unset DFX_NETWORK
 
-dfx canister create uploads
 dfx canister create internet_identity
+dfx build internet_identity 
+dfx canister install internet_identity 
+
+dfx canister create uploads
+dfx build uploads 
+dfx canister install uploads 
+
 dfx canister create frontend
 dfx canister create backend
 
 dfx build backend
 bash make_did.sh
 dfx build frontend
+
+exit 0
+
+
 dfx identity use default
-dfx canister install uploads 
-dfx canister install internet_identity 
 dfx canister install frontend 
 dfx canister install backend 
 cd src/backend

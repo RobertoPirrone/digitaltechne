d6d823a420b110bad0bf67061d8cdb53937fb17c
Roberto Pirrone <roberto.pirrone@most.it>
2024-09-24 18:59:17 +0200

    Tema azzurro

--- src/frontend/src/components/theme.jsx
+++ src/frontend/src/components/theme.jsx
@@ -1,19 +1,10 @@
 import { createTheme } from "@mui/material/styles";
+import { red, green, blue } from '@mui/material/colors';
+
 export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#E1E002",
-      contrastText: "#000000",
-    },
-    // secondary : usare button variant="outlined"
-    // per fare button con colore di sfondo, usare variant="contained", main: colore sfondo, contrastText: colore scritta
-    secondary: {
-      main: "#777700",
-    },
-    /*
-      background: {
-        default: '#999999',
-      },
-      */
-  },
+          palette: {
+            // type: 'dark',
+            primary: blue,
+            // secondary: { main: '#f48fb1', },
+          },
 });

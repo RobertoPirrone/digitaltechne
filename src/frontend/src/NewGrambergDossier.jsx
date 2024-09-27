f22ef7167c4a0d3110373f919da5dcbbd938d51b
Roberto Pirrone <roberto.pirrone@most.it>
2024-09-27 14:26:24 +0200

    tolta la richiesta di pulldown, non vengono usati

--- src/frontend/src/NewGrambergDossier.jsx
+++ src/frontend/src/NewGrambergDossier.jsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Autocomplete from "@mui/material/Autocomplete";
+import Typography from "@mui/material/Typography";
 import { v4 as uuidv4 } from 'uuid';
 import { XlsFile } from "./components/XlsFile";
 
@@ -55,33 +56,6 @@ export const NewDossier = () => {
 
   useEffect(() => {
     setSearchele(false);
-    if (backendActor === null) {
-      navigate("/login");
-    } else {
-      backendActor
-        .dossier_pulldowns()
-        .then((Ok_data) => {
-          console.log("dossieropera returns: ", JSON.stringify(Ok_data));
-          let data = JSON.parse(Ok_data.Ok);
-          let pulldowns = {
-            autori: data.autori,
-          };
-          console.log("struct pulldowns: ", JSON.stringify(pulldowns));
-          setNewDossierInfo(pulldowns);
-          if (lastInsert && lastInsert.what === "autore" && response["autore"]) {
-            response["autore"].forEach((r) => {
-              if (r.value === lastInsert.id) {
-                setValue("autore", r, { shouldValidate: true });
-                setLastInsert(null);
-              }
-            });
-          }
-        })
-        .catch(function (error) {
-          console.error(error);
-          appAlert(error.message ? error.message : JSON.stringify(error));
-        });
-    }
   }, [t, searchele, appAlert, lastInsert]);
 
   const actionChange = (e, el) => {
@@ -115,7 +89,7 @@ export const NewDossier = () => {
     } else {
       vals.private = true;
     }
-    vals.icon_uri = asset.key;
+    vals.icon_uri = assets[0].key;
     setDisabledButs(true);
     console.log("onSubmit: ", JSON.stringify(vals));
     backendActor
@@ -160,9 +134,9 @@ export const NewDossier = () => {
       )}
       <Container component="main" maxWidth="md">
         <div className={DTRoot}>
-          <Upload asset={asset} show={true} setAsset={setAsset} setDisabledButs={setDisabledButs} />
+          <UploadNew assets={assets} show={true} asset={assets[0]} setAssets={setAssets} setDisabledButs={setDisabledButs} label={t("dossier:LoadJpgs")} />
 
-          <form onSubmit={onSubmit}>
+          <form onSubmit={handleSubmit(onSubmit)} >
             <Grid container spacing={1} alignItems="center">
               <Grid item xs={12}> <MyTextField name="nomeopera" required={true} label={t("dossier:nomeopera")} onChange={(e) => setNomeOpera(e.target.value)} /> </Grid>
               <Grid item xs={12}> <SpecializedSelect defaultValue={""} name="tipotecnica" label={t("tipotecnica:Label")} what={"tipotecnica"} onChange={(e, v) => setTipotecnica(e.target.value)} /> </Grid>
@@ -172,7 +146,7 @@ export const NewDossier = () => {
               <Grid item xs={12}> <SpecializedSelect defaultValue={""} name="tipofirma" label={t("tipofirma:Label")} what={"tipofirma"} onChange={(e, v) => setTipofirma(e.target.value)} /> </Grid>
               <Grid item xs={12}> <SpecializedSelect defaultValue={""} name="tiposupporto" label={t("tiposupporto:Label")} what={"tiposupporto"} onChange={(e, v) => setTiposupporto(e.target.value)} /> </Grid>
 
-              <Grid item xs={3}> <text>Private</text> <MyCheckbox defaultChecked={false} onChange={(e, v) => setPrivateDossier(v.label)} /> </Grid>
+              <Grid item xs={3}> <Typography>Private</Typography> <MyCheckbox defaultChecked={false} onChange={(e, v) => setPrivateDossier(v.label)} /> </Grid>
               <Grid item xs={12}> {" "} &nbsp; </Grid>
               <MostSubmitButton disabled={disabledButs} label={t("dossier:Inserisci")} />
             </Grid>
@@ -196,15 +170,14 @@ export const BatchInsert = () => {
   const [assets, setAssets] = useState({});
   const { backendActor, principal } = useAuth();
 
-  const onBatchSubmit = (submit_vals) => {
-    let vals = {};
+  const onBatchSubmit = (vals) => {
     let file_found = false;
     vals.uuid = uuidv4();
     vals.insert_time = new Date();
     vals.username = "pippo";
 
     setDisabledButs(true);
-    console.log("onBatchSubmit vals: ", JSON.stringify(vals));
+    console.log("onBatchSubmit vals: ", vals);
     console.log("onBatchSubmit json: ", jsonText);
     let r = {};
     let ass = {};
@@ -273,10 +246,15 @@ export const BatchInsert = () => {
           vals.private = false;
 
         setDisabledButs(true);
-        console.log("onBatchSubmit dossier_insert: ", JSON.stringify(vals));
+        console.log("onBatchSubmit dossier_insert: ");
+        console.log( vals);
+        console.log("onBatchSubmit dossier_insert: ", vals);
+
         backendActor
           .dossier_insert(JSON.stringify(vals))
           .then((Ok_data) => {
+            console.error( "OKKKK");
+            console.log( Ok_data);
             console.log("dossier_insert no json returns: ", Ok_data);
             console.log("dossier_insert returns: ", JSON.stringify(Ok_data));
             let response = JSON.parse(Ok_data.Ok);
@@ -291,6 +269,7 @@ export const BatchInsert = () => {
             }
           })
           .catch(function (error) {
+            console.error( "CATCH");
             console.error(error);
             alert(error.message ? error.message : JSON.stringify(error));
             setDisabledButs(false);
@@ -309,11 +288,11 @@ export const BatchInsert = () => {
             <Grid container spacing={1} alignItems="center">
 
         <XlsFile sheetIndex={0} setJsonText={setJsonText} label={"Metadata file (.xlsx format)"}/>
-          <UploadNew assets={assets} show={true} setAssets={setAssets} setDisabledButs={setDisabledButs} label={t("dossier:LoadJpgs")} />
+          <UploadNew assets={assets} show={false} setAssets={setAssets} setDisabledButs={setDisabledButs} label={t("dossier:LoadJpgs")} />
                 <Grid item xs={12}> {" "} &nbsp; </Grid>
       </Grid>
 
-          <form onSubmit={onBatchSubmit}>
+          <form onSubmit={handleSubmit(onBatchSubmit)} >
             <Grid container spacing={1} alignItems="center">
 
               <Grid item xs={12}> {" "} &nbsp; </Grid>

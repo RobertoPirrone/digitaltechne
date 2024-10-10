6c84f69ad5fa2ddecee59619f06a1c7912267b43
Roberto Pirrone <roberto.pirrone@most.it>
2024-10-10 12:51:16 +0200

    biome

--- src/frontend/src/CartridgeInsert.jsx
+++ src/frontend/src/CartridgeInsert.jsx
@@ -1,133 +1,152 @@
-import React, { useContext, useState, useMemo, useEffect, useCallback } from "react";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import React, { useContext, useState, useMemo, useEffect, useCallback } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import * as XLSX from "xlsx";
 
-import { Header } from "./Header";
+import { backend } from "../../declarations/backend";
 import { Footer } from "./Footer";
-import { DTGrow, DTFooter } from "./components/useStyles";
-import { MyTextField, MyCheckbox, MyAutocomplete, MostSubmitButton, MostCheckbox, MostSelect, MostTextField } from "./components/MostComponents";
-import { useGlobalState } from "./state";
-import { DTRoot } from "./components/useStyles";
+import { Header } from "./Header";
 import { Upload } from "./Upload";
 import { appAlert } from "./Utils";
-import { XlsFile } from "./components/XlsFile";
-import { backend } from "../../declarations/backend";
 import { useAuth } from "./auth/use-auth-client";
+import { MostCheckbox, MostSelect, MostSubmitButton, MostTextField, MyAutocomplete, MyCheckbox, MyTextField } from "./components/MostComponents";
+import { XlsFile } from "./components/XlsFile";
+import { DTFooter, DTGrow } from "./components/useStyles";
+import { DTRoot } from "./components/useStyles";
+import { useGlobalState } from "./state";
 
+/*
+ * Inserimento nella tavola cartridge
+ *
+ * Obbligatorio fornire XLS, da cui viene ricavata il genoma in json
+ */
 export const CartridgeInsert = () => {
-  const { control, register, handleSubmit, watch, formState: { errors }, } = useForm();
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-  const [pdfAsset, setPdfAsset] = useState("");
-  const [disabledButs, setDisabledButs] = useState(true);
-  const [searchele, setSearchele] = useState(false);
-  const [note, setNote] = useState("");
-  const [dnaText, setDnaText] = useState("");
-  const [file, setFile] = useState(null);
-  const [jsonData, setJsonData] = useState("");
-  const [csvData, setCsvData] = useState("");
-  const [csvText, setCsvText] = useState("");
-  const [jsonText, setJsonText] = useState("");
-  // const [ whoami, setWhoami, backendActor, setBackendActor, assetPfx, setAssetPfx ] = useContext(myContext);
-  // const backendActor = getBackendActor();
-  // const whoami = "2vxsx-fae";
-  const { backendActor, whoami } = useAuth();
-
+    const {
+        control,
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const [pdfAsset, setPdfAsset] = useState("");
+    const [disabledButs, setDisabledButs] = useState(true);
+    const [searchele, setSearchele] = useState(false);
+    const [note, setNote] = useState("");
+    const [dnaText, setDnaText] = useState("");
+    const [file, setFile] = useState(null);
+    const [jsonData, setJsonData] = useState("");
+    const [csvData, setCsvData] = useState("");
+    const [csvText, setCsvText] = useState("");
+    const [jsonText, setJsonText] = useState("");
+    const { backendActor, whoami } = useAuth();
 
     const gotXls = (e) => {
         console.log("gotXls: ");
         setFile(e.target.files[0]);
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const data = e.target.result;
-        const workbook = XLSX.read(data, { type: "binary" });
-        const sheetName = workbook.SheetNames[1];
-        const worksheet = workbook.Sheets[sheetName];
-        const json = XLSX.utils.sheet_to_json(worksheet);
-          console.log("json: ", json);
-        const csv = XLSX.utils.sheet_to_csv(worksheet);
-          console.log("csv: ", csv);
-        setJsonText(JSON.stringify(json, null, 2));
-        setCsvText(csv);
-      };
-      reader.readAsBinaryString(e.target.files[0]);
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "binary" });
+            const sheetName = workbook.SheetNames[1];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);
+            console.log("json: ", json);
+            const csv = XLSX.utils.sheet_to_csv(worksheet);
+            console.log("csv: ", csv);
+            setJsonText(JSON.stringify(json, null, 2));
+            setCsvText(csv);
+        };
+        reader.readAsBinaryString(e.target.files[0]);
         setDisabledButs(false);
-    }
+    };
 
-  const onSubmit = (vals) => {
-    vals.uuid = uuidv4();
-    vals.dna_text = csvText;
-      if (pdfAsset == "" ) {
-        vals.dna_file_asset = "NO file";
-      } else {
-        vals.dna_file_asset = pdfAsset.key;
-      }
-    vals.lab_name = "Laboratorio CNR Catania";
-    vals.note = note;
-
-    vals.insert_time = new Date();
-    vals.username = whoami;
-
-    setDisabledButs(true);
-    console.log("onSubmit: ", JSON.stringify(vals));
-    backendActor
-      .cartridge_insert(JSON.stringify(vals))
-      .then((Ret_data) => {
-        console.log("cartridge_insert returns: ", JSON.stringify(Ret_data));
-        if ("Ok" in Ret_data) { 
-          let response = JSON.parse(Ret_data.Ok);
-          console.log(response);
-          setDisabledButs(true);
-          navigate("/dossier");
+    const onSubmit = (vals) => {
+        vals.uuid = uuidv4();
+        vals.dna_text = csvText;
+        if (pdfAsset === "") {
+            vals.dna_file_asset = "NO file";
         } else {
-          let err = Ret_data.Err;
-          console.error(err);
-          appAlert(err);
-          setDisabledButs(false);
+            vals.dna_file_asset = pdfAsset.key;
         }
-      })
-      .catch(function (error) {
-        console.error(error);
-        alert(error.message ? error.message : JSON.stringify(error));
-        setDisabledButs(false);
-      });
+        vals.lab_name = "Laboratorio CNR Catania";
+        vals.note = note;
 
-  }
+        vals.insert_time = new Date();
+        vals.username = whoami;
 
+        setDisabledButs(true);
+        console.log("onSubmit: ", JSON.stringify(vals));
+        backendActor
+            .cartridge_insert(JSON.stringify(vals))
+            .then((Ret_data) => {
+                console.log("cartridge_insert returns: ", JSON.stringify(Ret_data));
+                if ("Ok" in Ret_data) {
+                    const response = JSON.parse(Ret_data.Ok);
+                    console.log(response);
+                    setDisabledButs(true);
+                    navigate("/dossier");
+                } else {
+                    const err = Ret_data.Err;
+                    console.error(err);
+                    appAlert(err);
+                    setDisabledButs(false);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                alert(error.message ? error.message : JSON.stringify(error));
+                setDisabledButs(false);
+            });
+    };
 
-  return (
-    <>
-      <Header />
-      <h1>{t("CartridgeInsert")}</h1>
-      <Container component="main" maxWidth="md">
-        <div className={DTRoot}>
-            <Grid container spacing={1} alignItems="center">
+    return (
+        <>
+            <Header />
+            <h1>{t("CartridgeInsert")}</h1>
+            <Container component="main" maxWidth="md">
+                <div className={DTRoot}>
+                    <Grid container spacing={1} alignItems="center">
+                        <XlsFile setDisabledButs={setDisabledButs} setCsvText={setCsvText} setJsonText={setJsonText} />
+                        <Grid item xs={6}>
+                            {" "}
+                            <span className="padding10">{t("DnaFilePdf")}</span>
+                        </Grid>
+                        <Grid item xs={6}>
+                            {" "}
+                            <Upload accept={"application/pdf"} asset={pdfAsset} setAsset={setPdfAsset} setDisabledButs={setDisabledButs} show={false} />{" "}
+                        </Grid>
+                        <Grid item xs={12}>
+                            {" "}
+                            &nbsp;{" "}
+                        </Grid>
+                    </Grid>
 
-        <XlsFile setDisabledButs={setDisabledButs} setCsvText={setCsvText} setJsonText={setJsonText}/>
-                <Grid item xs={6}> <span className="padding10">{t("DnaFilePdf")}</span></Grid>
-                <Grid item xs={6}> <Upload accept={"application/pdf"} asset={pdfAsset} setAsset={setPdfAsset} setDisabledButs={setDisabledButs} show={false} /> </Grid>
-                <Grid item xs={12}> {" "} &nbsp; </Grid>
-      </Grid>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Grid container spacing={1} alignItems="center">
+                            <Grid item xs={12}>
+                                {" "}
+                                <MyTextField name="note" label={t("note")} onChange={(e) => setNote(e.target.value)} />{" "}
+                            </Grid>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={1} alignItems="center">
-              <Grid item xs={12}> <MyTextField name="note" label={t("note")} onChange={(e) => setNote(e.target.value)} /> </Grid>
+                            <Grid item xs={12}>
+                                {" "}
+                                &nbsp;{" "}
+                            </Grid>
 
-              <Grid item xs={12}> {" "} &nbsp; </Grid>
-
-              <MostSubmitButton disabled={disabledButs} label={t("dossier:Inserisci")} />
-            </Grid>
-          </form>
-        </div>
-      </Container>
-      <Footer />
-    </>
-  );
-}
+                            <MostSubmitButton disabled={disabledButs} label={t("dossier:Inserisci")} />
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+            <Footer />
+        </>
+    );
+};

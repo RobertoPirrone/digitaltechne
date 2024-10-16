2ac4ac29806947802d6a0a48bfc8b4b6a3028808
Roberto Pirrone <roberto.pirrone@most.it>
2024-10-16 17:22:31 +0200

    i miei primi test del backend!!
    probabilmente le cose in .../src/frontend/package.json non servono
    visto che i test si fanno dalla root del progetto

---
+++ src/tests/actor.js
@@ -0,0 +1,22 @@
+import { Actor, HttpAgent } from "@dfinity/agent";
+import fetch from "isomorphic-fetch";
+import canisterIds from ".dfx/local/canister_ids.json";
+import { idlFactory } from "../declarations/backend/backend.did.js";
+
+export const createActor = async (canisterId, options) => {
+  const agent = new HttpAgent({ ...options?.agentOptions });
+  await agent.fetchRootKey();
+
+  // Creates an actor with using the candid interface and the HttpAgent
+  return Actor.createActor(idlFactory, {
+    agent,
+    canisterId,
+    ...options?.actorOptions,
+  });
+};
+
+export const backendCanister = canisterIds.backend.local;
+
+export const backend = await createActor(backendCanister, {
+  agentOptions: { host: "http://127.0.0.1:4943", fetch },
+});

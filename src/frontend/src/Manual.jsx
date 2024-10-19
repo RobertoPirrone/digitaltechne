1c70f1f0759959564c9d617b5608a5553a06316d
Roberto Pirrone <roberto.pirrone@most.it>
2024-10-19 16:16:44 +0200

    Documentazione JS e Rust accessibile solo in locale
    mediante Copy&Paste di file:///...

--- src/frontend/src/Manual.jsx
+++ src/frontend/src/Manual.jsx
@@ -1,5 +1,8 @@
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import Typography from "@mui/material/Typography";
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -10,12 +13,21 @@ import { DTFooter, DTGrow } from "./components/useStyles";
 export const Manual = () => {
     const { t } = useTranslation();
 
+    const launch = (url) => {
+        window.open("file:///Users/robi/SRC/InternetComputer/gramberg/target/doc/backend/index.html");
+    }
     return (
         <>
             <Header />
             <h1>{t("Manual")}</h1>
-            <Container maxWidth="sm">
-                <Typography variant="body1">TBD</Typography>
+            <Container maxWidth="xl">
+                <Typography variant="body1">
+                    <List>
+                    <ListItem> <ListItemText primary="The Main user Manual" /> <Link color="inherit" href="/UserManual.pdf" target="_blank" rel="noopener"> /UserManual.pdf</Link>{" "} </ListItem>
+                    <ListItem> <ListItemText primary="Backend Implementation Notes" /> file:///Users/robi/SRC/InternetComputer/gramberg/target/doc/backend/index.html  </ListItem>
+                    <ListItem> <ListItemText primary="Frontend Implementation Notes" />file:///Users/robi/SRC/InternetComputer/gramberg/src/frontend/public/JSdocs/index.html  </ListItem>
+                    </List>
+                </Typography>
             </Container>
             <Footer />
         </>

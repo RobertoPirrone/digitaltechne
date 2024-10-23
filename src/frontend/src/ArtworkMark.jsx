22d1ce328bd508cd1987266b3404b8f9524dc0a4
Roberto Pirrone <roberto.pirrone@most.it>
2024-10-23 10:13:23 +0200

    eliminato Upload.jsx, solo più UploadNew

--- src/frontend/src/ArtworkMark.jsx
+++ src/frontend/src/ArtworkMark.jsx
@@ -15,7 +15,6 @@ import Grid from "@mui/material/Grid";
 import { backend } from "../../declarations/backend";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
-import { Upload } from "./Upload";
 import { UploadNew } from "./UploadNew";
 import { useAuth } from "./auth/use-auth-client";
 import { DocData } from "./components/DocData";

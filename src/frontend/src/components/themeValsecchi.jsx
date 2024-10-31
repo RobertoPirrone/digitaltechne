906fc766ccfa12d173c2a831fbf5bd334a2b3ae3
Roberto Pirrone <roberto.pirrone@most.it>
2024-10-31 10:45:05 +0100

    personalizzato tema

---
+++ src/frontend/src/components/themeValsecchi.jsx
@@ -0,0 +1,10 @@
+import { blue, green, red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        // type: 'dark',
+        primary: green,
+        // secondary: { main: '#f48fb1', },
+    },
+});

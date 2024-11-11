3e4d76829de03dde23dce81249d29074486c1b16
Roberto Pirrone <roberto.pirrone@most.it>
2024-11-11 18:31:10 +0100

    refactoring per tradurre più tipi file (json/xlsx)
    xlsx ancora da fare

---
+++ src/frontend/prepare_i18n/translate.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import json, sys,re,os
+# pip3 install google-cloud-translate==2.0.1
+# ricevo: 
+#   * lingua
+#   * file di output
+#   * Opzionale: righe del file italiano in più rispetto al vecchio tag git
+# quindi intervengo solo se:
+# la traduzione non esiste
+# la traduzione esiste già, ma in itialiano è cambiata la string
+# rimane il problema di righe eliminate del tutto, che rimangono nelle traduzioni
+
+from translate_json_file import json_tran
+from translate_xlsx_file import xlsx_tran
+
+if not (len(sys.argv) == 4 or len(sys.argv) == 5):
+    print("Uso: sys.argv[0] lang [JSON,XLSX] src_file_path [rows]")
+    print("target_file will be locales/lang/file")
+    exit (1)
+lang=sys.argv[1]
+file_type=sys.argv[2]
+src_file=sys.argv[3]
+
+if file_type == "JSON":
+    json_tran(src_file, lang)
+if file_type == "XLSX":
+    xlsx_tran(src_file, lang)

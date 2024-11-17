acdcc90614d478fed0def3c2a53ed3687a38b5ee
Roberto Pirrone <roberto.pirrone@most.it>
2024-11-17 19:06:59 +0100

    sempre doc

--- src/docs/make.sh
+++ src/docs/make.sh
@@ -1,6 +1,15 @@
 . .venv/bin/activate
 cd docs
 
+sphinx-build -D html_theme=alabaster -D html_theme_options.nosidebar=True . ./_build/html
+rm -rf ../frontend/public/html_en
+cp -a docs/_build/html ../frontend/public/
+
+sphinx-build -D master_doc=index_it -D html_theme=alabaster -D html_theme_options.nosidebar=True . ./_build/html_it
+rm -rf ../../frontend/public/html_it
+cp -a ./_build/html_it ../../frontend/public/
+exit 0
+
 sphinx-build -b html . ./rtd_build_en/html
 rm -rf ../../frontend/public/rtd_html_en
 cp -a ./rtd_build_en/html ../../frontend/public/rtd_html_en

a4f863a6682a9724742ebe419fc0dc2ee2122a15
Roberto Pirrone <roberto.pirrone@most.it>
2024-11-18 18:24:55 +0100

    r

--- remove_file_from_git.sh
+++ remove_file_from_git.sh
@@ -1,6 +1,6 @@
 # rimozione di un file dal repository e dalla history
 # cfr. https://nicolaiarocci.com/how-to-remove-a-file-from-git-history/
-PATH_TO_FILE=src/frontend/prepare_i18n/google_key.json
+PATH_TO_FILE=src/frontend/prepare_i18n/translate/google_key.json
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch $PATH_TO_FILE" \
   --prune-empty --tag-name-filter cat -- --all

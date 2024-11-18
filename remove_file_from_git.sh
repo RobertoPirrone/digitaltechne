da53290dc968f9f58592d67f86f3cbd195f9bca0
Roberto Pirrone <roberto.pirrone@most.it>
2024-11-18 17:47:18 +0100

    r

---
+++ remove_file_from_git.sh
@@ -0,0 +1,9 @@
+# rimozione di un file dal repository e dalla history
+PATH_TO_FILE=src/frontend/prepare_i18n/google_key.json
+git filter-branch --force --index-filter \
+  "git rm --cached --ignore-unmatch $PATH_TO_FILE" \
+  --prune-empty --tag-name-filter cat -- --all
+
+# push
+git push origin --force --all
+
